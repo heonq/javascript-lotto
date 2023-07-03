7ecfb327c83c4e5fd46b13b83e9b198bf51b2a5f
heonq <gkagjsrb@naver.com>
2023-07-03 13:25:17 +0900

    feat: 로또 메시지 및 에러 메시지 상수 추가

--- constant/constant.js
+++ constant/constant.js
@@ -1,5 +1,23 @@
 const LOTTO_MESSAGE = {
-  AMOUNT: '구입 금액을 입력해주세요.',
+  INPUT_AMOUNT: '구입 금액을 입력해주세요.',
+  PRINT_QTY: '개를 구매하셨습니다.',
+  INPUT_MAIN_NUMBERS: '당첨 번호를 입력해 주세요.',
+  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  PRINT_RESULT: '당첨 통계\n---',
 };
 
-module.exports(LOTTO_MESSAGE);
+const ERROR_MESSAGE = {
+  AMOUNT: '[ERROR] 로또 구매 금액은 1,000원 단위로 입력해주세요.',
+  MAIN_NUBER: {
+    NOT_IN_RANGE: '[ERROR] 1부터 45 사이의 숫자를 입력해주세요.',
+    DUPLICATED: '[ERROR] 중복되지 않은 6개의 숫자를 입력해주세요.',
+    NOT_SIX: '[ERROR] 6개의 숫자를 입력해주세요.',
+    MISSING_COMMA: '[ERROR] 콤마(,)를 이용해 숫자를 구분해주세요.',
+  },
+  BONUS_NUMBER: {
+    DUPLICATED: '[ERROR] 당첨번호와 중복되지 않는 숫자를 입력해주세요.',
+    NOT_IN_RANGE: '[ERROR] 1부터 45 사이의 숫자를 입력해주세요.',
+  },
+};
+
+module.exports(LOTTO_MESSAGE, ERROR_MESSAGE);
--- src/User.js
+++ src/User.js
@@ -3,7 +3,7 @@ const { LOTTO_MESSAGE } = require('../constant/constant');
 
 class User {
   getUserPurchaseAmount() {
-    readLine(LOTTO_MESSAGE.AMOUNT, (amount) => {
+    readLine(LOTTO_MESSAGE.INPUT_AMOUNT, (amount) => {
       this.amount = amount;
     });
   }
