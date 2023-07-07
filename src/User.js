78d8b4ce53d7e5c1c190d6b820b15d950b39198c
heonq <gkagjsrb@naver.com>
2023-07-07 11:51:57 +0900

    feat: 구매한 수량만큼 로또를 발행하고 발행된 로또 출력하는 기능 구현

--- src/User.js
+++ src/User.js
@@ -1,11 +1,18 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 const { LOTTO_MESSAGE } = require('../constants/lottoMessage');
 const { LOTTO } = require('../constants/lottoValue');
 const Validator = require('../lib/Validator');
+const Lotto = require('./Lotto');
 
 class User {
+  constructor() {
+    this.#storage = [];
+  }
+
   play() {
     this.purchaseLotto();
+    this.generateLotto();
+    this.printLotto();
   }
 
   purchaseLotto() {
@@ -15,6 +22,27 @@ class User {
       this.quantity = this.amount / LOTTO.PRICE;
       Console.print(this.quantity + LOTTO_MESSAGE.PRINT_QUANTITY);
     });
+  }
+  generateLotto() {
+    for (let i = 0; i < this.quantity; i++) {
+      const lottoNumber = Random.pickUniqueNumbersInRange(
+        LOTTO.MIN_NUMBER,
+        LOTTO.MAX_NUMBER,
+        LOTTO.NUMBER_COUNT,
+      ).sort((a, b) => a - b);
+      const lotto = new Lotto(lottoNumber);
+      this.#storage.push(lotto);
+    }
+  }
+  printLotto() {
+    Console.print(getLottoString());
+  }
+
+  getLottoString() {
+    this.#storage.reduce((stringConsole, lotto) => {
+      let lottoString = lotto.join(',');
+      return stringConsole + `[${lottoString}]\n`;
+    }, '');
   }
 }
 
