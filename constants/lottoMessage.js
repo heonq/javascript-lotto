afd7a6cd8dfaf74c29da764dd12aa5c53bfc391a
heonq <gkagjsrb@naver.com>
2023-07-08 17:21:19 +0900

    feat: 에러 메시지 상수 추가

--- constants/lottoMessage.js
+++ constants/lottoMessage.js
@@ -1,23 +1,27 @@
 const LOTTO_MESSAGE = Object.freeze({
   INPUT_AMOUNT: '구입금액을 입력해 주세요.\n',
-  PRINT_QUANTITY: '개를 구매했습니다.',
-  INPUT_MAIN_NUMBER: '당첨 번호를 입력해 주세요.',
-  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  PRINT_QUANTITY: '개를 구매했습니다.\n',
+  INPUT_MAIN_NUMBER: '당첨 번호를 입력해 주세요.\n',
+  INPUT_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
   PRINT_RESULT: '당첨 통계\n---',
-  QUATITY: '개',
+  QUANTITY: '개\n',
   THREE_SAME: '3개 일치 (5,000원) - ',
   FOUR_SAME: '4개 일치 (50,000원) - ',
   FIVE_SAME: '5개 일치 (1,500,000원) - ',
   FIVE_BONUS_SAME: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
   SIX_SAME: '6개 일치 (2,000,000,000원) - ',
   RATE_OF_RETURN: '총 수익률은 ',
-  IS: '%입니다.',
+  IS: '%입니다.\n',
 });
 
 const ERROR_MESSAGE = Object.freeze({
   NOT_THOUSAND_UNIT: '1,000원 단위로 금액을 입력해주세요.',
   LESS_THAN_THOUSAND: '1,000원 이상의 금액을 입력해주세요.',
   NOT_NUMBER: '숫자를 입력해주세요.',
+  NOT_SIX_DIGITS: '6개의 숫자를 입력해주세요.',
+  OUT_OF_RANGE: '1에서 45 사이의 숫자를 입력해주세요.',
+  DUPLICATED_NUMBER: '중복되지 않은 숫자를 입력해주세요.',
+  MISSING_COMMA: ',를 이용해 숫자를 구분해주세요.',
 });
 
 module.exports = { LOTTO_MESSAGE, ERROR_MESSAGE };
