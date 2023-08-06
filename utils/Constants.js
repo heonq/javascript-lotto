e05b99b1e921a8295e14babbac6628b6bac3bab9
heonq <gkagjsrb@naver.com>
2023-08-06 22:42:09 +0900

    feat: Constants 파일에 상수 저장하는 객체 추가 - LOTTO_PRIZE : 일치하는 숫자의 수에 따른 등수 저장 - PRIZE_AMOUNT : 등수에 따른 상금 저장

--- utils/Constants.js
+++ utils/Constants.js
@@ -3,6 +3,23 @@ const CONSTANTS = Object.freeze({
   minNumber: 1,
   maxNumber: 45,
   lottoPrice: 1000,
+  prizeMinimumCount: 3,
+  checkBonusCount: 5,
 });
 
-module.exports = CONSTANTS;
+const LOTTO_PRIZE = Object.freeze({
+  3: 'fifthPrize',
+  4: 'fourthPrize',
+  5: 'thirdPrize',
+  6: 'firstPrize',
+});
+
+const PRIZE_AMOUNT = Object.freeze({
+  fifthPrize: 5_000,
+  fourthPrize: 50_000,
+  thirdPrize: 1_500_000,
+  secondPrize: 30_000_000,
+  firstPrize: 2_000_000_000,
+});
+
+module.exports = { CONSTANTS, LOTTO_PRIZE, PRIZE_AMOUNT };
