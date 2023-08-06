b15af47d46a645e3f96f463af6ef62199d89d1a3
heonq <gkagjsrb@naver.com>
2023-08-06 14:24:03 +0900

    feat: 당첨 번호에 대한 유효성 검사 기능 구현

--- utils/Validator.js
+++ utils/Validator.js
@@ -1,9 +1,17 @@
 const ERROR_MESSAGE = require('./ErrorMessage');
+const CONSTANTS = require('./Constants');
 
 const Validator = {
   validatePurchase(amount) {
     if (+amount < 1000) throw new Error(ERROR_MESSAGE.lessThanThousand);
     if (+amount % 1000 !== 0) throw new Error(ERROR_MESSAGE.thousandUnit);
+  },
+  validateLottoNumber(numbers) {
+    if (numbers.length !== CONSTANTS.validLength) throw new Error(ERROR_MESSAGE.invalidLength);
+    if (new set([...numbers]).size !== CONSTANTS.validLength)
+      throw new Error(ERROR_MESSAGE.duplicated);
+    if (numbers.every((number) => CONSTANTS.minNumber <= number && number <= CONSTANTS.maxNumber))
+      throw new Error(ERROR_MESSAGE.outOfRange);
   },
 };
 
