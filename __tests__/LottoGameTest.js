9b3010b25173dc23f31a86317b8b22a65f509810
heonq <gkagjsrb@naver.com>
2023-08-07 12:37:21 +0900

    test(LottoGame): 구매한 로또에 대한 문자열 출력 테스트 구현

---
+++ __tests__/LottoGameTest.js
@@ -0,0 +1,38 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const LottoGame = require('../src/LottoGame');
+const { CONSTNATS, CONSTANTS } = require('../utils/Constants');
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickUniqueNumbersInRange);
+};
+
+describe('로또 게임 테스트', () => {
+  test('구매한 로또에 대한 문자열 출력 테스트', () => {
+    const lottoGame = new LottoGame();
+    mockRandoms([
+      [8, 21, 23, 41, 42, 43],
+      [3, 5, 11, 16, 32, 38],
+      [7, 11, 16, 35, 36, 44],
+      [1, 8, 11, 31, 41, 42],
+      [13, 14, 16, 38, 42, 45],
+      [7, 11, 30, 40, 42, 43],
+      [2, 13, 22, 32, 38, 45],
+      [1, 3, 5, 14, 22, 45],
+    ]);
+    const logs = [
+      '[8, 21, 23, 41, 42, 43]',
+      '[3, 5, 11, 16, 32, 38]',
+      '[7, 11, 16, 35, 36, 44]',
+      '[1, 8, 11, 31, 41, 42]',
+      '[13, 14, 16, 38, 42, 45]',
+      '[7, 11, 30, 40, 42, 43]',
+      '[2, 13, 22, 32, 38, 45]',
+      '[1, 3, 5, 14, 22, 45]',
+    ];
+    lottoGame.generateLotto(logs.length * CONSTANTS.lottoPrice);
+    expect(lottoGame.getNumbersString()).toEqual(expect.stringContaining(logs.join('\n')));
+  });
+});
