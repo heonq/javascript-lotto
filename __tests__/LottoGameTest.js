881bb8eebb6c64d0b086ac3899cd6341a8ac3a0a
heonq <gkagjsrb@naver.com>
2023-08-07 13:26:52 +0900

    test: 로또 당첨 결과와 수익률 구하는 기능 테스트 구현

--- __tests__/LottoGameTest.js
+++ __tests__/LottoGameTest.js
@@ -35,4 +35,28 @@ describe('로또 게임 테스트', () => {
     lottoGame.generateLotto(logs.length * CONSTANTS.lottoPrice);
     expect(lottoGame.getNumbersString()).toEqual(expect.stringContaining(logs.join('\n')));
   });
+  test('로또 당첨 결과와 수익률 구하는 기능 테스트', () => {
+    const lottoGame = new LottoGame();
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+    mockRandoms([
+      [1, 2, 3, 8, 9, 10],
+      [1, 2, 3, 4, 9, 10],
+      [1, 2, 3, 4, 5, 9],
+      [1, 2, 3, 4, 5, 7],
+      [1, 2, 3, 4, 5, 6],
+    ]);
+    const log = [
+      '3개 일치 (5,000원) - 1개',
+      '4개 일치 (50,000원) - 1개',
+      '5개 일치 (1,500,000원) - 1개',
+      '5개 일치, 보너스 볼 일치 (30,000,000원) - 1개',
+      '6개 일치 (2,000,000,000원) - 1개',
+      '총 수익률은 40631100%입니다.',
+    ];
+
+    lottoGame.generateLotto(5000);
+    lottoGame.handlePrize(winningNumbers, bonusNumber);
+    expect(lottoGame.getStatistics()).toEqual(expect.stringContaining(log.join('\n')));
+  });
 });
